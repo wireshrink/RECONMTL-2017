c979c809ab8ad8ce05e34ef0e565339893c3ec38
wireshrink <wireshrink@gmail.com>
2017-06-04 13:38:32 +0300

    Initial version of unsealing

--- DVSE/SGXFileReader.cpp
+++ DVSE/SGXFileReader.cpp
@@ -1,5 +1,6 @@
 #include "SGXFileReader.h"
 #include "DVSE_t.h" 
+#include "SGXIndependentSealing.h"
 
 // Constructors/Destructors
 //  
@@ -34,15 +35,45 @@ bool SGXFileReader::open(char filename[1024])
 	m_pos = 0;
 	return true;
 }
-
+#define _MIN(a, b) (a)<(b)? (a):(b);
 size_t SGXFileReader::read(unsigned char * buffer, size_t buffer_size)
 {
-	return size_t();
+	// calculating real position in encrypted file
+	size_t enc_pos = SGXIndependentSealing::calc_sealed_data_size(m_pos);
+	size_t current_chunk_pos = (enc_pos / SGXIndependentSealing::SEALED_DATA_CHUNK_SIZE) * SGXIndependentSealing::SEALED_DATA_CHUNK_SIZE;
+	size_t read_data = 0;
+	size_t chunk_read_data = 0;
+	unsigned char sealed_chunk[SGXIndependentSealing::SEALED_DATA_CHUNK_SIZE];
+	unsigned char *unsealed = nullptr;
+	size_t        unsealed_size = 0;
+	int retval;
+	do
+	{
+		//reading the current chunk
+		sgx_status_t ret = ocall_file_read(&retval, getHandle(), current_chunk_pos, SGXIndependentSealing::SEALED_DATA_CHUNK_SIZE, sealed_chunk);
+		if (ret != SGX_SUCCESS)
+			return -1L;
+		if (retval <= 0)
+		{
+			return -1L;
+		}
+		if (!SGXIndependentSealing::unseal_data(sealed_chunk, retval, &unsealed, &unsealed_size))
+			return -1L;
+		size_t off_in_chunk = m_pos % SGXIndependentSealing::UNSEALED_DATA_CHUNK_SIZE;
+		size_t size_in_chunk = _MIN(buffer_size, (SGXIndependentSealing::UNSEALED_DATA_CHUNK_SIZE - off_in_chunk));
+		memcpy(buffer, unsealed + off_in_chunk, size_in_chunk);
+		read_data += size_in_chunk;
+		m_pos += size_in_chunk;
+		enc_pos = SGXIndependentSealing::calc_sealed_data_size(m_pos);
+		current_chunk_pos = (enc_pos / SGXIndependentSealing::SEALED_DATA_CHUNK_SIZE) * SGXIndependentSealing::SEALED_DATA_CHUNK_SIZE;
+		SGXIndependentSealing::destroy_allocated_data(unsealed);
+	} while (retval != 0 && read_data < buffer_size);
+	return read_data;
 }
 
 size_t SGXFileReader::write(unsigned char * buffer, size_t buffer_size)
 {
-	return -1L;
+	return -1L; // no write no cry
 }
 
 bool SGXFileReader::close()
@@ -54,17 +85,3 @@ bool SGXFileReader::close()
 	setHandle(nullptr);
 	return true;
 }
-
-//  
-// Methods
-//  
-
-
-// Accessor methods
-//  
-
-
-// Other methods
-//  
-
-
