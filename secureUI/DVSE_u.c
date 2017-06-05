8684cbc0c3664044ef33e6dbdfe3eaa8eeaa02b3
wireshrink <wireshrink@gmail.com>
2017-06-05 09:49:00 +0300

    Untrusted viewer project adjustments for SGX OpenSSL

--- secureUI/DVSE_u.c
+++ secureUI/DVSE_u.c
@@ -116,6 +116,70 @@ typedef struct ms_ocall_get_the_current_time_t {
 	unsigned char* ms_thetime;
 } ms_ocall_get_the_current_time_t;
 
+typedef struct ms_u_sgxssl_ftime64_t {
+	void* ms_timeptr;
+	uint32_t ms_timeb64Len;
+} ms_u_sgxssl_ftime64_t;
+
+typedef struct ms_u_sgxssl_closesocket_t {
+	int ms_retval;
+	void* ms_s;
+	int* ms_wsaError;
+} ms_u_sgxssl_closesocket_t;
+
+typedef struct ms_u_sgxssl_recv_t {
+	int ms_retval;
+	void* ms_s;
+	void* ms_buf;
+	int ms_len;
+	int ms_flag;
+	int* ms_wsaError;
+} ms_u_sgxssl_recv_t;
+
+typedef struct ms_u_sgxssl_send_t {
+	int ms_retval;
+	void* ms_s;
+	char* ms_buf;
+	int ms_len;
+	int ms_flags;
+	int* ms_wsaError;
+} ms_u_sgxssl_send_t;
+
+typedef struct ms_u_sgxssl_shutdown_t {
+	int ms_retval;
+	void* ms_s;
+	int ms_how;
+	int* ms_wsaError;
+} ms_u_sgxssl_shutdown_t;
+
+typedef struct ms_sgx_oc_cpuidex_t {
+	int* ms_cpuinfo;
+	int ms_leaf;
+	int ms_subleaf;
+} ms_sgx_oc_cpuidex_t;
+
+typedef struct ms_sgx_thread_wait_untrusted_event_ocall_t {
+	int ms_retval;
+	void* ms_self;
+} ms_sgx_thread_wait_untrusted_event_ocall_t;
+
+typedef struct ms_sgx_thread_set_untrusted_event_ocall_t {
+	int ms_retval;
+	void* ms_waiter;
+} ms_sgx_thread_set_untrusted_event_ocall_t;
+
+typedef struct ms_sgx_thread_setwait_untrusted_events_ocall_t {
+	int ms_retval;
+	void* ms_waiter;
+	void* ms_self;
+} ms_sgx_thread_setwait_untrusted_events_ocall_t;
+
+typedef struct ms_sgx_thread_set_multiple_untrusted_events_ocall_t {
+	int ms_retval;
+	void** ms_waiters;
+	size_t ms_total;
+} ms_sgx_thread_set_multiple_untrusted_events_ocall_t;
+
 static sgx_status_t SGX_CDECL DVSE_ocall_file_open(void* pms)
 {
 	ms_ocall_file_open_t* ms = SGX_CAST(ms_ocall_file_open_t*, pms);
@@ -196,11 +260,91 @@ static sgx_status_t SGX_CDECL DVSE_ocall_get_the_current_time(void* pms)
 	return SGX_SUCCESS;
 }
 
+static sgx_status_t SGX_CDECL DVSE_u_sgxssl_ftime64(void* pms)
+{
+	ms_u_sgxssl_ftime64_t* ms = SGX_CAST(ms_u_sgxssl_ftime64_t*, pms);
+	u_sgxssl_ftime64(ms->ms_timeptr, ms->ms_timeb64Len);
+
+	return SGX_SUCCESS;
+}
+
+static sgx_status_t SGX_CDECL DVSE_u_sgxssl_closesocket(void* pms)
+{
+	ms_u_sgxssl_closesocket_t* ms = SGX_CAST(ms_u_sgxssl_closesocket_t*, pms);
+	ms->ms_retval = u_sgxssl_closesocket(ms->ms_s, ms->ms_wsaError);
+
+	return SGX_SUCCESS;
+}
+
+static sgx_status_t SGX_CDECL DVSE_u_sgxssl_recv(void* pms)
+{
+	ms_u_sgxssl_recv_t* ms = SGX_CAST(ms_u_sgxssl_recv_t*, pms);
+	ms->ms_retval = u_sgxssl_recv(ms->ms_s, ms->ms_buf, ms->ms_len, ms->ms_flag, ms->ms_wsaError);
+
+	return SGX_SUCCESS;
+}
+
+static sgx_status_t SGX_CDECL DVSE_u_sgxssl_send(void* pms)
+{
+	ms_u_sgxssl_send_t* ms = SGX_CAST(ms_u_sgxssl_send_t*, pms);
+	ms->ms_retval = u_sgxssl_send(ms->ms_s, (const char*)ms->ms_buf, ms->ms_len, ms->ms_flags, ms->ms_wsaError);
+
+	return SGX_SUCCESS;
+}
+
+static sgx_status_t SGX_CDECL DVSE_u_sgxssl_shutdown(void* pms)
+{
+	ms_u_sgxssl_shutdown_t* ms = SGX_CAST(ms_u_sgxssl_shutdown_t*, pms);
+	ms->ms_retval = u_sgxssl_shutdown(ms->ms_s, ms->ms_how, ms->ms_wsaError);
+
+	return SGX_SUCCESS;
+}
+
+static sgx_status_t SGX_CDECL DVSE_sgx_oc_cpuidex(void* pms)
+{
+	ms_sgx_oc_cpuidex_t* ms = SGX_CAST(ms_sgx_oc_cpuidex_t*, pms);
+	sgx_oc_cpuidex(ms->ms_cpuinfo, ms->ms_leaf, ms->ms_subleaf);
+
+	return SGX_SUCCESS;
+}
+
+static sgx_status_t SGX_CDECL DVSE_sgx_thread_wait_untrusted_event_ocall(void* pms)
+{
+	ms_sgx_thread_wait_untrusted_event_ocall_t* ms = SGX_CAST(ms_sgx_thread_wait_untrusted_event_ocall_t*, pms);
+	ms->ms_retval = sgx_thread_wait_untrusted_event_ocall((const void*)ms->ms_self);
+
+	return SGX_SUCCESS;
+}
+
+static sgx_status_t SGX_CDECL DVSE_sgx_thread_set_untrusted_event_ocall(void* pms)
+{
+	ms_sgx_thread_set_untrusted_event_ocall_t* ms = SGX_CAST(ms_sgx_thread_set_untrusted_event_ocall_t*, pms);
+	ms->ms_retval = sgx_thread_set_untrusted_event_ocall((const void*)ms->ms_waiter);
+
+	return SGX_SUCCESS;
+}
+
+static sgx_status_t SGX_CDECL DVSE_sgx_thread_setwait_untrusted_events_ocall(void* pms)
+{
+	ms_sgx_thread_setwait_untrusted_events_ocall_t* ms = SGX_CAST(ms_sgx_thread_setwait_untrusted_events_ocall_t*, pms);
+	ms->ms_retval = sgx_thread_setwait_untrusted_events_ocall((const void*)ms->ms_waiter, (const void*)ms->ms_self);
+
+	return SGX_SUCCESS;
+}
+
+static sgx_status_t SGX_CDECL DVSE_sgx_thread_set_multiple_untrusted_events_ocall(void* pms)
+{
+	ms_sgx_thread_set_multiple_untrusted_events_ocall_t* ms = SGX_CAST(ms_sgx_thread_set_multiple_untrusted_events_ocall_t*, pms);
+	ms->ms_retval = sgx_thread_set_multiple_untrusted_events_ocall((const void**)ms->ms_waiters, ms->ms_total);
+
+	return SGX_SUCCESS;
+}
+
 static const struct {
 	size_t nr_ocall;
-	void * func_addr[10];
+	void * func_addr[20];
 } ocall_table_DVSE = {
-	10,
+	20,
 	{
 		(void*)(uintptr_t)DVSE_ocall_file_open,
 		(void*)(uintptr_t)DVSE_ocall_file_close,
@@ -212,6 +356,16 @@ static const struct {
 		(void*)(uintptr_t)DVSE_ocall_socket_receive,
 		(void*)(uintptr_t)DVSE_ocall_socket_shutdown,
 		(void*)(uintptr_t)DVSE_ocall_get_the_current_time,
+		(void*)(uintptr_t)DVSE_u_sgxssl_ftime64,
+		(void*)(uintptr_t)DVSE_u_sgxssl_closesocket,
+		(void*)(uintptr_t)DVSE_u_sgxssl_recv,
+		(void*)(uintptr_t)DVSE_u_sgxssl_send,
+		(void*)(uintptr_t)DVSE_u_sgxssl_shutdown,
+		(void*)(uintptr_t)DVSE_sgx_oc_cpuidex,
+		(void*)(uintptr_t)DVSE_sgx_thread_wait_untrusted_event_ocall,
+		(void*)(uintptr_t)DVSE_sgx_thread_set_untrusted_event_ocall,
+		(void*)(uintptr_t)DVSE_sgx_thread_setwait_untrusted_events_ocall,
+		(void*)(uintptr_t)DVSE_sgx_thread_set_multiple_untrusted_events_ocall,
 	}
 };
 
--- secureUI/DVSE_u.h
+++ secureUI/DVSE_u.h
@@ -24,6 +24,16 @@ int SGX_UBRIDGE(SGX_NOCONVENTION, ocall_socket_send, (void* data, size_t data_si
 int SGX_UBRIDGE(SGX_NOCONVENTION, ocall_socket_receive, (void* data, size_t data_size));
 int SGX_UBRIDGE(SGX_NOCONVENTION, ocall_socket_shutdown, ());
 int SGX_UBRIDGE(SGX_NOCONVENTION, ocall_get_the_current_time, (unsigned char thetime[16]));
+void SGX_UBRIDGE(SGX_NOCONVENTION, u_sgxssl_ftime64, (void* timeptr, uint32_t timeb64Len));
+int SGX_UBRIDGE(SGX_NOCONVENTION, u_sgxssl_closesocket, (void* s, int* wsaError));
+int SGX_UBRIDGE(SGX_NOCONVENTION, u_sgxssl_recv, (void* s, void* buf, int len, int flag, int* wsaError));
+int SGX_UBRIDGE(SGX_NOCONVENTION, u_sgxssl_send, (void* s, const char* buf, int len, int flags, int* wsaError));
+int SGX_UBRIDGE(SGX_NOCONVENTION, u_sgxssl_shutdown, (void* s, int how, int* wsaError));
+void SGX_UBRIDGE(SGX_CDECL, sgx_oc_cpuidex, (int cpuinfo[4], int leaf, int subleaf));
+int SGX_UBRIDGE(SGX_CDECL, sgx_thread_wait_untrusted_event_ocall, (const void* self));
+int SGX_UBRIDGE(SGX_CDECL, sgx_thread_set_untrusted_event_ocall, (const void* waiter));
+int SGX_UBRIDGE(SGX_CDECL, sgx_thread_setwait_untrusted_events_ocall, (const void* waiter, const void* self));
+int SGX_UBRIDGE(SGX_CDECL, sgx_thread_set_multiple_untrusted_events_ocall, (const void** waiters, size_t total));
 
 sgx_status_t ecall_init_enclave(sgx_enclave_id_t eid, int* retval, char* storage_folder, char* address, int port);
 sgx_status_t ecall_update_epg(sgx_enclave_id_t eid, int* retval);
